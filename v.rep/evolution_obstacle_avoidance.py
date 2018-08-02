fixed normalization of values; Hopefully;

v.rep/evolution_obstacle_avoidance.py
@@ -88,12 +88,12 @@ def evolution_obstacle_avoidance():
 
             individual.loop()
 
-            # print(individual)
+            print(individual)
 
             fitness_t = np.append(
                 (individual.wheel_speeds[0] +
                  individual.wheel_speeds[1] / 2) *
-                (5 - np.sqrt(np.absolute(
+                (1 - np.sqrt(np.absolute(
                     individual.wheel_speeds[0] -
                     individual.wheel_speeds[1]))) *
                 (1 - np.amin(individual.sensor_activation)),

v.rep/robot.py
@@ -132,7 +132,7 @@ class EvolvedRobot(Robot):
             else:
                 wheelspeed += np.int16(
                     np.array(self.chromosome[i * 4 + 2:i * 4 + 4]) + wheel_bias)
-                self.sensor_activation = np.append(self.sensor_activation, 0)
+                self.sensor_activation = np.append(self.sensor_activation, self.get_sensor_distance(sensor))
 
         # normalize sensor data in range [0, 1]
         # self.sensor_activation = normalize(self.sensor_activation[:,np.newaxis], axis=0)