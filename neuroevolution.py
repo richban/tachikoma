543b06318f28ecf28331b979ef792c72f6d2587a
richban <rbanyi@me.com>
2018-11-22 14:16:38 +0100

    don't show fitness of each individual in prompt;

--- neuroevolution.py
+++ neuroevolution.py
@@ -159,7 +159,7 @@ def run(config_file, args):
             # Before closing the connection to V-REP, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
             vrep.simxGetPingTime(client_id)
 
-            print('%s with fitness: %f and distance %f' % (str(id), fitness, fitness_aff[0]))
+            # print('%s with fitness: %f and distance %f' % (str(id), fitness, fitness_aff[0]))
 
             if (vrep.simxStopSimulation(client_id, OP_MODE) == -1):
                 print('Failed to stop the simulation\n')
